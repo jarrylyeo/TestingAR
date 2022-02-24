6e6d178c74e7789ef4d72f596cb67ce140ed400f
jarrylyeo <jarrylyeo@gmail.com>
2022-02-24 22:11:28 +0800

    minor edits to change lat long

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
        {
            name: 'Magnemite',
            location: {
-               lat: 44.496470,
-               lng: 11.320180,
+               lat: 1.379570,
+               lng: 103.938150,
            }
        },
    ];
